c5b34d3817009c120b802844c0370f1a8c66ea92
AigerimAdr <aigerim210596@gmail.com>
2023-08-05 00:36:26 +0600

    Выполнила ДЗ-7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleTaskChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleAddTask = (event) => {
+    if (task.trim() !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleToggleTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = `~${tasks[index]}~`;
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-todo-list">
+      <h1>TODO LIST</h1>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={handleTaskChange}
+          placeholder="Enter task"
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span
+              style={{
+                textDecoration: task.startsWith('~') && task.endsWith('~') ? 'line-through' : 'none',
+              }}
+            >
+              {task.replace(/~/g, '')}
+            </span>
+            <span className='change_buttons'>
+              <button className='change__btn' onClick={() => handleToggleTask(index)}>
+                {task.startsWith('~') && task.endsWith('~') ? 'Uncheck' : 'Check'}
+              </button>
+              <button className='change__btn' onClick={() => handleDeleteTask(index)}>Delete</button>
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
